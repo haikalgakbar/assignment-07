07e3f72d4b192010586eb482ad92fba77b346bd6
haikalgakbar <haikalgakbar@gmail.com>
2024-05-23 12:55:23 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# React + TypeScript + Vite + TailwindCSS + React-Router-DOM
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-slate-100 px-4 py-2 bg-blue-500">
+        Hello from react starter code
+      </h1>
+    </>
+  );
+}
