df2352622cd2d4abe5f60db1f08ea8f7da6ad4bf
haikalgakbar <haikalgakbar@gmail.com>
2024-06-01 22:53:15 +0700

    configure vite

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,11 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 3001,
+    host: true,
+  },
 });
